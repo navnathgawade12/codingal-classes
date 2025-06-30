d4c4bff7034b5a4afc37594affaa2a3069588822
navnathgawade12 <navnathgawade12@gmail>
2025-06-30 18:50:07 +0530

    so many errors

---
+++ lesson 25/activity1.js
@@ -0,0 +1,40 @@
+var a = 92
+var b = 12.08
+var c = "I CAN SMELL YOU"
+var d = null
+var e = undefined
+var f = true 
+var g = false
+var h = {Map : "earth" , place : "asia" , day : "monday"}
+
+console.log(typeof(c))
+
+var i = new Date()
+var j = new Date().getHours()
+console.log(i)
+console.log(j)
+
+while(a < 120){
+    console.log("im dead but alive")
+    a = a+2
+}
+
+let y = 21
+const n = 14
+
+
+{
+    var w = 63
+}
+
+console.log("a man without cnfidence is like a box" , w)
+
+let s = "im not the man of sadness"
+
+console.log("hello everyone", s)
+
+const j = "fbi here to find you"
+
+j = "fbi is not finding you"
+
+console.log(j)
