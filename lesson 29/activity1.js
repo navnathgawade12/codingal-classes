a86fd828fdb7e076daf50fcae22dd75b7bc39ab8
navnathgawade12 <navnathgawade12@gmail>
2025-07-14 18:52:42 +0530

    i dont want to wait

--- lesson 29/activity1.js
+++ lesson 29/activity1.js
@@ -1,12 +1,12 @@
-var person = {name:"raghav", age: 14 , city: "pune"}
+// var person = {name:"raghav", age: 14 , city: "pune"}
 
-var m = JSON.stringify(person)
+// var m = JSON.stringify(person)
 
-var person2 = JSON.parse(m)
+// var person2 = JSON.parse(m)
 
-console.log(m)
+// console.log(m)
 
-document.getElementById("oh damn").innerHTML = person2.city + "  " + person2.name
+// document.getElementById("oh damn").innerHTML = person2.city + "  " + person2.name
 
 function getphotos(){
     return Promise ((resolve) => {setTimeout(()=> {console.log("YOU ARE ALSO A VILLAN IN SOMEONES STORY") }, 5000) })
