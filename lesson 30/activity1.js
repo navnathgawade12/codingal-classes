9cd0d0edb37af697c4c5babe341a25658374469b
navnathgawade12 <navnathgawade12@gmail>
2025-07-18 18:32:36 +0530

    I RANDOMLY PRINTED SOMETHING

---
+++ lesson 30/activity1.js
@@ -0,0 +1,9 @@
+function validatecode(){
+    var a = document.getElementById
+    ("damn").value
+    if(a == ""){
+        alert("dont be scared its just a job application form")
+    }
+
+    console.log("filling the job application")
+}
