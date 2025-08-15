3ad4556a592a449de353e2bc908f31e83a67c5b1
navnathgawade12 <navnathgawade12@gmail>
2025-08-15 22:09:02 +0530

    more calculator

--- lesson34/script.js
+++ lesson34/script.js
@@ -20,6 +20,7 @@ numberbutton.forEach(button=>{
 operatorbuttons.forEach(button=>{
     button.addEventListener('click', ()=>{
         const id = button.id
+        let result
         switch(id){
             case 'clear':
                 currentinput = ''
@@ -27,6 +28,68 @@ operatorbuttons.forEach(button=>{
                 operator = null
                 updatedisplay('')
                 break;
+                case 'backspace':
+                    currentinput = currentinput.slice(0,-1)
+                    updatedisplay(currentinput)
+                    break
+                    case 'equals':
+                        if(operator && previousinput && currentinput){
+                            result = calculate(parseFloat(previousinput) , parseFloat(currentinput) , operator)
+                            updatedisplay(result.tostring())
+                            currentinput = result.tostring()
+                            previousinput = ''
+                            operator = null
+                        }
+                        break
+                        case 'divide':
+                        case 'multiply':
+                        case 'subtract':
+                        case 'sum':
+                            if(currentinput){
+                                if(previousinput && operator){
+                                    result = calculate(parseFloat(previousinput) , parseFloat(currentinput) , operator)
+                                    previousinput = result.tostring()
+                                    updatedisplay(result)
+                                }else{
+                                    previousinput = currentinput
+                                    currentinput = ''
+                                    operator = getoperatorsymbol(id)
+                                }
+                            }
+                            break
         }
     })
 })
+
+function getoperatorsymbol(id){
+    switch(id){
+        case 'divide':
+            return '/'
+        case 'multiply':
+            return '*'
+        case 'subtract':
+            return '-'
+        case 'sum':
+            return '+'
+            default:
+            return ''
+    }
+}
+
+function calculate(a,b,op){
+    switch(op){
+        case "+":
+            return a+b
+        case "-":
+            return a-b
+        case "*":
+            return a*b
+        case "/":
+            return a/b
+        default:
+            return b
+    }
+}
+
+
+updatedisplay("")
