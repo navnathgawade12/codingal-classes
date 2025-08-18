c29cfb3b96ee3ecbc8bb971ae6924eae44733e3d
navnathgawade12 <navnathgawade12@gmail>
2025-08-18 18:24:34 +0530

    welcome to python

---
+++ python/lesson1/demo.py
@@ -0,0 +1,12 @@
+# this is a comment that , this computer ugnores so bad
+
+print("THERES NO QUEEN OF ENGLAND")
+
+print(63262)
+
+a = "A JOB"
+b = 3723872848
+c = True
+
+print(b)
+print(f'ONE OF HUMANITIES BIGGEST FEAR {a}')
