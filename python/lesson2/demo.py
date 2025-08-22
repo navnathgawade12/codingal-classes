1f724c6f11d60c759c7984ca16271e54d463f129
navnathgawade12 <navnathgawade12@gmail>
2025-08-22 18:31:21 +0530

    slicing the code

---
+++ python/lesson2/demo.py
@@ -0,0 +1,25 @@
+a = "IM IN YOUR WALLS"
+b = 77628872
+c = True
+d = 2388761.11988
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+b = str(b)
+print(type(b))
+
+print(3**1)
+print(5**8)
+
+z = input("hello")
+print(z)
+print(type(z))
+
+z = int(z)
+
+n = 'i got burgers in the back'
+
+print(n[0:13])
