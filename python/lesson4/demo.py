dccb5d34215f2698edb1abda6efe22e546aed995
navnathgawade12 <navnathgawade12@gmail>
2025-08-29 18:33:59 +0530

    loops in py

---
+++ python/lesson4/demo.py
@@ -0,0 +1,18 @@
+for a in range(1, 101)
+    print("OPTIMUS")
+    print("PRIME")
+    print(a)
+
+    x=0
+    while x < 20:
+        print("while loop running")
+        if x == 3:
+            x = 21
+            x= x + 1
+
+m = [1, 2, 3, 4, 5]
+n = ['a', 'b', 'c','d', 'e']
+
+for o in m:
+    for p in n:
+        print(o,p)
