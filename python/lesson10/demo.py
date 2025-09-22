677f1f5a8e40f3e174af0e264178461f40a82e1b
navnathgawade12 <navnathgawade12@gmail>
2025-09-22 15:33:02 +0530

    expression

---
+++ python/lesson10/demo.py
@@ -0,0 +1,20 @@
+class manager:
+    def __init__(self,name,salary):
+        print("a worker class")
+    self.name = name
+    self.salary = salary
+
+def salaryday(self):
+    print('your salary which is has been credited to your bank account')
+
+def __del__():{
+    ("destructor hit")
+}    
+
+a1 = manager('mahesh', 45000)
+del a1
+
+a2 = manager('tv repair', 30000)
+
+a1.salaryday()
+a2.salaryday()
