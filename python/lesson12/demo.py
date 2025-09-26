91ad4d87acb3f5e79dce5ddfc0c17c687ca46056
navnathgawade12 <navnathgawade12@gmail>
2025-09-26 18:33:28 +0530

    keep account safe

---
+++ python/lesson12/demo.py
@@ -0,0 +1,34 @@
+class circle:
+    def __init__(self,r):
+        self.r = r
+
+
+    def area(self):
+        return 3.14 * self.r * self.r
+
+class square:
+    def __init__(self,r):
+        self.s = s 
+
+    def area(self):
+        return self.r * self.r 
+
+c1 = circle(s)
+s1 = square(s)
+
+print(c1.area())
+print(s1.area())
+
+class account:
+    def __init__(self):
+        self.__balance(self)
+
+    def check_balance(self):
+        return self.balance 
+
+
+m1 = account()
+
+print(m1.check_balance())
+
+yash = account()
