1289f2d833f165d78eb733525212a6e0f7436102
navnathgawade12 <navnathgawade12@gmail>
2025-09-26 18:03:23 +0530

    finish the job

--- python/lesson11/demo.py
+++ python/lesson11/demo.py
@@ -18,3 +18,25 @@ class fruitvegetables:
 m1 = fruitvegetables('lemon', 5 ,'lime yellow', 'sour')
 
 m1.eatvegetables()
+
+
+from abc import ABC 
+
+class game(ABC):
+    def get_minecraft_game(self):
+    pass
+
+    def play_the_game(self):
+        pass
+
+class minecraft(game):
+    def get_minecraft_game(self):
+        print("go to the store")
+        print("get the minecraft game")
+        print("pay and take the game")
+
+    def play_the_game(self):
+        print("bought minecraft")
+        print("found a village")
+        print("burned the village")
+        print("got home and played minecraft")
