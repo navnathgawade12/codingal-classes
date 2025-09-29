01147a95777a91753c7c8cba35a34c4b35897583
navnathgawade12 <navnathgawade12@gmail>
2025-09-29 18:18:20 +0530

    file handling

---
+++ python/lesson13/demo.py
@@ -0,0 +1,13 @@
+file = open("el macho.text",'r')
+
+print(file.read())
+
+file('el macho.text','w')
+
+file.write('he was ruthless he was dangerous and as the name implies very macho.')
+
+file = open('el mach.text','a')
+
+file.write('but as all the greats he wa gone too soon.')
+
+file.close()
---
+++ python/lesson13/demo2.py
@@ -0,0 +1,3 @@
+file = open('el macho2.text','a')
+
+file.write('he died in the most macho way possible')
