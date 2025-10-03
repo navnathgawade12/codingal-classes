dc2057d8092640480fde73b6296a678c99df7dc8
navnathgawade12 <navnathgawade12@gmail>
2025-10-03 17:51:40 +0530

    recorrection

--- python/lesson13/demo.py
+++ python/lesson13/demo.py
@@ -1,12 +1,12 @@
-file = open("el macho.text",'r')
+file = open("python\lesson13\elmacho.txt",'r')
 
 print(file.read())
 
-file('el macho.text','w')
+file = open('python\lesson13\elmacho.txt','w')
 
 file.write('he was ruthless he was dangerous and as the name implies very macho.')
 
-file = open('el mach.text','a')
+file = open('python\lesson13\elmacho.txt','a')
 
 file.write('but as all the greats he wa gone too soon.')
 
